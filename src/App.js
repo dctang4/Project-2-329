96dfffd11fd4efdecd1af10baba3d6924ffeceb8
Chun Hin D. Tang <dtang955472@gmail.com>
2021-04-29 09:53:53 -0400

    start of testing api for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react"
+import Router from "react-router-dom"
 import './App.css';
 
 function App() {
+
+  const [heroesData, setHeroesData] = useState(null)
+
+
+
+  const getData = async () => {
+    
+    const url = "https://myheroacademiaapi.com/api/character";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setHeroesData(data.result)
+    
+
+    // for (let i = 2; i <= 16; i++) {
+    //   const url = `https://myheroacademiaapi.com/api/character?page=${i}`;
+
+    //   const response = await fetch(url);
+    //   const data = await response.json();
+
+    //   setHeroesData([...heroesData, ...data.result])
+    // }
+  }
+  useEffect(() => {getData()}, [])
+  console.log(...heroesData)
+
+  // const getData2 = async () => {
+  //   for (let i = 2; i <= 16; i++) {
+  //     const url = `https://myheroacademiaapi.com/api/character?page=${i}`;
+
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+
+  //     setHeroesData([...heroesData, ...data.result])
+  //   }
+  // }
+
+  
+  // useEffect(() => {getData2()}, [])
+  // console.log(heroesData)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
